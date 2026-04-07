fbc5f73fdd9a0cb4713907fffef56fc9f83492bb
subhasis04 <subhasispanda04@gmail.com>
2026-04-07 10:37:54 +0530

    version1

---
+++ number.py
@@ -0,0 +1,15 @@
+x = 24 
+# Check the remainder dividing x by 2 is 0
+if x % 2 == 0:
+    print("Even")
+else:
+    print("Odd")
+
+# Checking another number    
+x = 7
+
+if x % 2 == 0:
+
+  print("Even")
+else:
+    print("Odd")
